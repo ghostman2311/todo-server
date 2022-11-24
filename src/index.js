a378128690a47f485548a65fe9851a50a56095d3
Nikhil <nkarnwal07@gmail.com>
2022-11-24 22:54:01 +0530

    fetch api created

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import dbo from "./db/connection";
 
 const app = express();
-const port = process.env.PORT || 5000;
-dotenv.config({ path: "./config.env" });
-app.use(cors());
-app.use(express.json());
-// app.use(require("./routes/record"));
-// get driver connection
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
+const notes = [
+  {
+    id: "1",
+    title: "My First note",
+    content: "Hey there this is my first note",
+  },
+];
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on 8080");
 });
