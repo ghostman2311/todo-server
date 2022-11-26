3443a1e5a34148f0c7160a0bc71645116487250a
Nikhil <nkarnwal07@gmail.com>
2022-11-26 15:10:20 +0530

    added mongodb to backend

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from "express";
-
+import { v4 as uuid } from "uuid";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+// mongodb+srv://mongodb:<password>@project.3usro.mongodb.net/?retryWrites=true&w=majority
 const app = express();
+dotenv.config();
 
-const notes = [
+let notes = [
   {
     id: "1",
     title: "My First note",
@@ -10,10 +14,50 @@ const notes = [
   },
 ];
 
-app.get("/notes", (req, res) => {
-  res.json(notes);
-});
+const start = async () => {
+  const client = await MongoClient.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-app.listen(8080, () => {
-  console.log("Server is running on 8080");
-});
+  const noteDb = await client.db("note-app-db").collections("notes");
+  console.log(noteDb);
+  app.use(express.json());
+
+  app.get("/notes", (req, res) => {
+    res.json(notes);
+  });
+
+  app.post("/notes", (req, res) => {
+    const { title } = req.body;
+    notes.push({
+      title,
+      id: uuid(),
+      content: "",
+    });
+
+    res.json(notes);
+  });
+
+  app.put("/notes/:noteID", (req, res) => {
+    const { noteID } = req.params;
+    const { title, content } = req.body;
+    notes = notes.map((note) =>
+      note.id === noteID ? { id: noteID, title, content } : note
+    );
+
+    res.json(notes);
+  });
+
+  app.delete("/notes/:noteID", (req, res) => {
+    const { noteID } = req.params;
+    notes = notes.filter((note) => note.id !== noteID);
+    res.json(notes);
+  });
+
+  app.listen(8080, () => {
+    console.log("Server is running on 8080");
+  });
+};
+
+start();
