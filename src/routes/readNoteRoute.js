bf9d2bd61685ce7861778d9284539ae7963d9e91
Nikhil <nkarnwal07@gmail.com>
2022-12-04 14:55:13 +0530

    protected the listnote route

--- src/routes/readNoteRoute.js
+++ src/routes/readNoteRoute.js
@@ -1,15 +1,26 @@
 import { noteDb, userDb } from "../db";
+import * as admin from "firebase-admin";
 
 const readNoteRoute = {
   path: "/users/:userId/notes",
   method: "get",
   handler: async (req, res) => {
-    const { userId } = req.params;
-    const user = await userDb.findOne({ authID: userId });
-    const notes = await Promise.all(
-      user.notes.map((id) => noteDb.findOne({ id }))
-    );
-    res.json(notes);
+    try {
+      const { authtoken } = req.headers;
+      const authUser = await admin.auth().verifyIdToken(authtoken);
+      const { userId } = req.params;
+      if (authUser.uid !== userId) {
+        res.sendStatus(403);
+      }
+      const user = await userDb.findOne({ authID: userId });
+      const notes = await Promise.all(
+        user.notes.map((id) => noteDb.findOne({ id }))
+      );
+      res.json(notes);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(401);
+    }
   },
 };
 
